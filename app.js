3c1cade6c59b945304dc59bc7559a317852d7d2c
bonnah john <bonnahpoku@gmail.com>
2022-11-24 11:43:05 -0800

    navBar is build for now

---
+++ app.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelector(".links");
+const burger = document.querySelector(".burger");
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("picker");
+});
+
+window.addEventListener("scroll", () => {
+  var nav = document.querySelector("nav");
+  nav.classList.toggle("change", screenY > 0);
+});
